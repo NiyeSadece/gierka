418a80a55d8bb94b311956fc0d3ad33bc6e53b7e
NiyeSadece <39556362+NiyeSadece@users.noreply.github.com>
2020-12-20 19:16:45 +0100

    day 2

--- game_app/models.py
+++ game_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    description = models.TextField()
+    short_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.short_description
